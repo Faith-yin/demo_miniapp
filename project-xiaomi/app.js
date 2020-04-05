623c53209813222eb07bc1884a954d73aa223ed1
yinpengfei <1207828715@qq.com>
2020-04-05 15:29:43 +0800

    我的页面 -- 1，底部弹窗css3弹窗动画实现；2.关闭X符号右对齐【谨记text-align对行内元素起作用，对块级元素无效】

--- project-xiaomi/app.js
+++ project-xiaomi/app.js
@@ -4,9 +4,14 @@ App({
     navHeight: null, //导航栏总高度
     navTop: null, //导航栏距离顶部高度
     windowHeight: null, //当前设备总高度
+    userInfo: {}, //用户信息
   },
+  /**
+   * 监听小程序初始化
+   */
   onLaunch: function() {
     let menuButtonObj = wx.getMenuButtonBoundingClientRect()
+    // 获取设备信息
     wx.getSystemInfo({
       success: res => {
         let statusBarHeight = res.statusBarHeight
@@ -19,8 +24,28 @@ App({
         this.globalData.windowHeight = res.windowHeight
       },
       fail: err => {
-        console.log(err)
+        console.log('获取设备信息出错',err)
       },
-    })
+    });
+    // 获取用户当前的授权状态
+    wx.getSetting({
+      withSubscriptions: true,
+      success: res => {
+        let getUserInfo = res.authSetting["scope.userInfo"]
+        if(getUserInfo) { // 已授权用户信息
+          wx.getUserInfo({
+            success: res => {
+              this.userInfo = res.userInfo
+            },
+            fail: err => {
+              console.log('获取用户信息失败',err)
+            }
+          })
+        }
+      },
+      fail: err => {
+        console.log('当前用户未授权',err)
+      }
+    });
   },
 })
--- project-xiaomi/pages/mine/mine.js
+++ project-xiaomi/pages/mine/mine.js
@@ -1,18 +1,132 @@
 // pages/mine/mine.js
+
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},           //用户信息
+    list: [                 //cell列表
+      {iconPath: '/assets/images/店铺，门店.png', text: '我的订单'},
+      {iconPath: '/assets/images/店铺，门店.png', text: '会员中心'},
+      {iconPath: '/assets/images/店铺，门店.png', text: '耗材服务'},
+      {iconPath: '/assets/images/店铺，门店.png', text: '我的优惠券'},
+      {iconPath: '/assets/images/店铺，门店.png', text: '我的F码'},
+      {iconPath: '/assets/images/店铺，门店.png', text: '我的礼物码'},
+      {iconPath: '/assets/images/店铺，门店.png', text: '我的红包'},
+      {iconPath: '/assets/images/店铺，门店.png', text: '在线客服'},
+      {iconPath: '/assets/images/店铺，门店.png', text: '设置'},
+    ],  
+    showDiolg: false,       //底部弹出层开关
+    showMsgDiolog: false,   //中间弹窗开关
   },
+
+  /**
+   * 点击登录账号
+   */
+  loginClick: function() {
+    this.setData({
+      showDiolg: true,
+    })
+  },
+  /**
+   * 微信一键登录  重新授权
+   */
+  getInfoClick: function() {
+    // 若中间弹窗是开的，要先关闭
+    if(this.data.showMsgDiolog) {
+      this.setData({
+        showMsgDiolog: false
+      })
+    }
+    // 向用户发起授权请求
+    wx.authorize({
+      scope: 'scope.userInfo',
+      success: res => {
+        console.log('获取用户信息成功',res)
+      },
+      fail: err => {
+        // console.log('获取用户信息失败',err)
+      }
+    })
+  },
+  /**
+   * 获取用户信息回调
+   */
+  bindGetUserInfo: function(e) {
+    if(!e.detail.userInfo) { //若用户点击取消申请信息 
+      this.setData({
+        showMsgDiolog: true
+      })
+      return;
+    }
+    this.setData({ //点击允许了
+      userInfo: e.detail.userInfo,
+      showDiolg: false,
+      showMsgDiolog: false
+    })
+  },
+  /**
+   * 关闭底部弹窗
+   */
+  closeDiolg: function() {
+    let {userInfo} = this.data
+    // 如期窗口关闭时没有获取到信息
+    if(!userInfo  || !Object.keys(userInfo).length) {
+      wx.showToast({
+        title: '登录后才可完成购买哦~',
+        icon: 'none'
+      })
+    }
+    this.setData({
+      showDiolg: false,
+    })
+  },
+  /**
+   * 先去转转
+   */
+  judgeHomePage: function() {
+    // 关闭中间弹窗
+    this.setData({
+      showMsgDiolog: false
+    })
+    // 跳首页
+    wx.switchTab({
+      url: '/pages/home/home',
+    })
+  },
+
+
+
+
+
+
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    if(app || Object.keys(app.userInfo).length) {
+      this.setData({
+        userInfo: app.userInfo
+      })
+    }
 
+    // let userInfo = wx.getStorage({
+    //   key: 'userInfo',
+    //   success: res => {
+    //     this.setData({
+    //       userInfo: userInfo
+    //     })
+    //   },
+    //   fail: err => {
+    //     console.log('获取用户信息失败',err)
+    //   }
+    // })
   },
 
   /**
@@ -26,7 +140,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
@@ -40,7 +154,7 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    
   },
 
   /**
